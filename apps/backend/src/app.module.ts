cd6c6976f3b1d6e699d68859356786864c6912cf
ticinovicsara <ticinovicsara0@gmail.com>
2025-05-05 22:53:20 +0200

    add enrollment folder w files

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
+import { GuestEnrollmentModule } from './guest-enrollment/guest-enrollment.module';
+import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
-  imports: [],
+  imports: [GuestEnrollmentModule, EnrollmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
