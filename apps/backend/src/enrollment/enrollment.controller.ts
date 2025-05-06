4a8205160edcbbed2bece0ad8832c8ca7a88b70a
ticinovicsara <ticinovicsara0@gmail.com>
2025-05-06 11:13:13 +0200

    add enrollment endpoints and services

--- apps/backend/src/enrollment/enrollment.controller.ts
+++ apps/backend/src/enrollment/enrollment.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { EnrollmentService } from './enrollment.service';
 import { RequestEnrollmentDto } from './dto/request-enrollment.dto';
 
 @Controller('enrollment')
 export class EnrollmentController {
   constructor(private readonly enrollmentService: EnrollmentService) {}
+
+  @Get('pending')
+  async getPendingRequests() {
+    return this.enrollmentService.getPendingEnrollmentRequests();
+  }
+
+  @Get('candidate/:candidateId')
+  async getCandidateEnrollmentRequests(
+    @Param('candidateId') candidateId: string,
+  ) {
+    return this.enrollmentService.getCandidateEnrollmentRequests(candidateId);
+  }
+
+  @Get('school/:schoolId')
+  async getSchoolEnrollmentRequests(@Param('schoolId') schoolId: string) {
+    return this.enrollmentService.getSchoolEnrollmentRequests(schoolId);
+  }
 
   @Post()
   async requestEnrollment(@Body() body: RequestEnrollmentDto) {
--- apps/backend/src/enrollment/enrollment.service.ts
+++ apps/backend/src/enrollment/enrollment.service.ts
@@ -11,8 +11,79 @@ import { RequestEnrollmentDto } from './dto/request-enrollment.dto';
 export class EnrollmentService {
   constructor(private prisma: PrismaService) {}
 
+  async getPendingEnrollmentRequests() {
+    const pendingRequests = await this.prisma.enrollmentRequest.findMany({
+      where: { status: EnrollmentStatus.Pending },
+      include: {
+        school: true,
+        candidate: true,
+      },
+    });
+
+    return pendingRequests.map((request) => ({
+      requestId: request.id,
+      candidateName: `${request.candidate.firstName} ${request.candidate.lastName}`,
+      schoolName: request.school.name,
+      status: request.status,
+    }));
+  }
+
+  async getCandidateEnrollmentRequests(candidateId: string) {
+    const requests = await this.prisma.enrollmentRequest.findMany({
+      where: { candidateId },
+      include: {
+        school: true,
+        candidate: true,
+      },
+    });
+
+    if (!requests.length) {
+      throw new NotFoundException(
+        'No enrollment requests found for this candidate.',
+      );
+    }
+
+    return requests.map((request) => ({
+      candidateName: `${request.candidate.firstName} ${request.candidate.lastName}`,
+      role: request.candidate.role,
+      schoolName: request.school.name,
+      status: request.status,
+    }));
+  }
+
+  async getSchoolEnrollmentRequests(schoolId: string) {
+    const requests = await this.prisma.enrollmentRequest.findMany({
+      where: { schoolId },
+      include: {
+        school: true,
+        candidate: true,
+      },
+    });
+
+    if (!requests.length) {
+      throw new NotFoundException(
+        'No enrollment requests found for this school.',
+      );
+    }
+
+    return requests.map((request) => ({
+      schoolName: request.school.name,
+      candidateName: `${request.candidate.firstName} ${request.candidate.lastName}`,
+      role: request.candidate.role,
+      status: request.status,
+    }));
+  }
+
   async requestEnrollment(body: RequestEnrollmentDto) {
     const { candidateId, schoolId } = body;
+
+    const candidate = await this.prisma.user.findUnique({
+      where: { id: candidateId },
+    });
+
+    if (!candidate) {
+      throw new NotFoundException('User is not found.');
+    }
 
     const existing = await this.prisma.enrollmentRequest.findUnique({
       where: {
