9c3386707db853356f948293fdd417020e8274c5
Luka <llesko00@fesb.hr>
2025-05-07 19:57:29 +0200

    Add endpoints for getting candidates and instructors sessions

--- apps/backend/src/session/session.module.ts
+++ apps/backend/src/session/session.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { PrismaService } from '@/prisma/prisma.service';
+import { UserService } from '@/user/user.service';
 
 @Module({
   controllers: [SessionController],
-  providers: [SessionService, PrismaService],
+  providers: [SessionService, PrismaService, UserService],
 })
 export class SessionModule {}
