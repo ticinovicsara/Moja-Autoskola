11bb3c62a859a48a07c3c030ae8eab171c2d6b21
Gadza03 <mgadza@pmfst.hr>
2025-05-10 01:59:16 +0200

    Home page test

--- apps/frontend/src/pages/HomePage.tsx
+++ apps/frontend/src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
+import { routes } from "@/constants";
+import { useAuth } from "@/contexts/AuthContext/useAuth";
+import { Navigate } from "react-router-dom";
+
 const HomePage = () => {
-    return <>Home</>;
+  const { user } = useAuth();
+  return user ? (
+    <h1>Welcome back on home page {user?.firstName} 👋</h1>
+  ) : (
+    <Navigate to={routes.LOGIN} />
+  );
 };
 
 export default HomePage;
