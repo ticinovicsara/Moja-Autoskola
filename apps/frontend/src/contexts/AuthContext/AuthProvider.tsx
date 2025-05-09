2c76e721aa1fb7314fe874c9fdcc9899c63ecaf7
Gadza03 <mgadza@pmfst.hr>
2025-05-09 23:39:59 +0200

    Auth context, fix payload

---
+++ apps/frontend/src/contexts/AuthContext/AuthProvider.tsx
@@ -0,0 +1,35 @@
+import { getUserFromToken, JwtPayload } from "@/utils/decodeUser";
+import { useEffect, useState } from "react";
+import { AuthContext } from "./authContext";
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<JwtPayload | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const userData = getUserFromToken(token);
+      if (userData) setUser(userData);
+      else logout();
+    }
+    setIsLoading(false);
+  }, []);
+
+  const login = (token: string) => {
+    localStorage.setItem("token", token);
+    const userData = getUserFromToken(token);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ apps/frontend/src/utils/decodeUser.ts
@@ -0,0 +1,28 @@
+import { UserRoles } from "@/enums";
+import { jwtDecode } from "jwt-decode";
+
+export type JwtPayload = {
+  id: string;
+  email: string;
+  role: UserRoles;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+  oib: string;
+  exp: number;
+};
+
+export const getUserFromToken = (token: string): JwtPayload | null => {
+  try {
+    const decoded = jwtDecode<JwtPayload>(token);
+    const now = Date.now() / 1000;
+
+    if (decoded.exp && decoded.exp < now) {
+      return null;
+    }
+
+    return decoded;
+  } catch (error) {
+    return null;
+  }
+};
