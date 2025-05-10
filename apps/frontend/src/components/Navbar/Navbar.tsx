4578a1a922545ff2ebfb87bfeab49b289ebab61c
Luka <llesko00@fesb.hr>
2025-05-10 11:56:43 +0200

    Use common container for navbar

--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
-        <nav className={styles.navbar}>
+        <nav className={`container ${styles.navbar}`}>
             <img src={Hamburger} alt="hamburger" />
             <img src={Logo} alt="logo" />
             <img src={Profile} alt="profile" className={styles.profile} />
