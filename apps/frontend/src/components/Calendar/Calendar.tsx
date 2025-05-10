d0d5ea53244d1812a5003caf8fcefe4e12898c36
Luka <llesko00@fesb.hr>
2025-05-10 19:13:10 +0200

    Style the calendar

--- apps/frontend/src/components/Calendar/Calendar.tsx
+++ apps/frontend/src/components/Calendar/Calendar.tsx
@@ -1,5 +1,5 @@
 import ReactCalendar from "react-calendar";
-import "react-calendar/dist/Calendar.css"; // Import default styles
+import "react-calendar/dist/Calendar.css";
 import styles from "./Calendar.module.css";
 
 const Calendar = () => {
@@ -7,10 +7,45 @@ const Calendar = () => {
         return ["N", "P", "U", "S", "Č", "P", "S"][date.getDay()];
     };
 
+    const formatMonthYear = (_locale: string | undefined, date: Date) => {
+        const croatianMonths = [
+            "Siječanj",
+            "Veljača",
+            "Ožujak",
+            "Travanj",
+            "Svibanj",
+            "Lipanj",
+            "Srpanj",
+            "Kolovoz",
+            "Rujan",
+            "Listopad",
+            "Studeni",
+            "Prosinac",
+        ];
+
+        return `${croatianMonths[date.getMonth()]} ${date.getFullYear()}`;
+    };
+
+    const tileClassName = ({ date, view }: { date: Date; view: string }) => {
+        if (view !== "month") return null;
+
+        if (date.getDay() === 0) {
+            return styles.sundayTile;
+        }
+
+        return null;
+    };
+
     return (
         <ReactCalendar
             className={`${styles.calendar} container`}
             formatShortWeekday={formatShortWeekday}
+            formatMonthYear={formatMonthYear}
+            showNeighboringMonth={false}
+            tileClassName={tileClassName}
+            tileDisabled={() => false}
+            next2Label={null}
+            prev2Label={null}
         />
     );
 };
