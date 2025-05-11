443c7d29a3c8655088247bafbd64f961833df357
Luka <llesko00@fesb.hr>
2025-05-11 14:55:16 +0200

    Style marked days

--- apps/frontend/src/components/Calendar/Calendar.tsx
+++ apps/frontend/src/components/Calendar/Calendar.tsx
@@ -2,13 +2,20 @@ import ReactCalendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import styles from "./Calendar.module.css";
 
+const markedDates = [
+    { date: new Date(2025, 4, 15), type: "FirstAid" },
+    { date: new Date(2025, 4, 16), type: "FirstAid" },
+    { date: new Date(2025, 4, 20), type: "Driving" },
+    { date: new Date(2025, 4, 25), type: "Theory" },
+];
+
 const Calendar = () => {
     const formatShortWeekday = (_locale: string | undefined, date: Date) => {
         return ["N", "P", "U", "S", "Č", "P", "S"][date.getDay()];
     };
 
     const formatMonthYear = (_locale: string | undefined, date: Date) => {
-        const croatianMonths = [
+        const months = [
             "Siječanj",
             "Veljača",
             "Ožujak",
@@ -23,19 +30,34 @@ const Calendar = () => {
             "Prosinac",
         ];
 
-        return `${croatianMonths[date.getMonth()]} ${date.getFullYear()}`;
+        return `${months[date.getMonth()]} ${date.getFullYear()}`;
     };
 
+    const isMarkedDate = (date: Date) => {
+        return markedDates.find(
+            (markedDate) =>
+                markedDate.date.getDate() === date.getDate() &&
+                markedDate.date.getMonth() === date.getMonth() &&
+                markedDate.date.getFullYear() === date.getFullYear()
+        );
+    };
     const tileClassName = ({ date, view }: { date: Date; view: string }) => {
         if (view !== "month") return null;
 
+        const classes = [];
+
         if (date.getDay() === 0) {
-            return styles.sundayTile;
+            classes.push(styles.sundayTile);
         }
 
-        return null;
-    };
+        const markedDate = isMarkedDate(date);
+        if (markedDate) {
+            classes.push("markedDateTile");
+            classes.push(`markedType-${markedDate.type}`);
+        }
 
+        return classes.length ? classes.join(" ") : null;
+    };
     return (
         <ReactCalendar
             className={`${styles.calendar} container`}
