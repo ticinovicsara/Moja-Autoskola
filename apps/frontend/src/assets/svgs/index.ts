3993d96f1be7c4a666e582dd860c8969a75c05fe
Luka <llesko00@fesb.hr>
2025-05-11 19:59:46 +0200

    Change navbar design

---
+++ apps/frontend/src/assets/svgs/index.ts
@@ -0,0 +1,5 @@
+import Hamburger from "./Hamburger.svg";
+import Logo from "./Logo.svg";
+import Arrow from "./Arrow.svg";
+
+export { Hamburger, Logo, Arrow };
--- apps/frontend/src/components/Navbar/Navbar.tsx
+++ apps/frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,11 @@
-import { Hamburger, Logo, Profile } from "@/assets/images";
+import { Hamburger, Logo } from "@/assets/svgs";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
         <nav className={`container ${styles.navbar}`}>
-            <img src={Hamburger} alt="hamburger" />
             <img src={Logo} alt="logo" />
-            <img src={Profile} alt="profile" className={styles.profile} />
+            <img src={Hamburger} alt="hamburger" />
         </nav>
     );
 };
