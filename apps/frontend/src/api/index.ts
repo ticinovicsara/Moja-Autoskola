786689776e85937b5f41d3594d495df17875117c
ticinovicsara <ticinovicsara0@gmail.com>
2025-05-14 17:38:22 +0200

    remove double exports

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -2,7 +2,5 @@ export * from "./instructor";
 export * from "./candidate";
 export * from "./session";
 export * from "./auth";
-export * from "./candidate";
-export * from "./instructor";
 export * from "./school";
 export * from "./api";
