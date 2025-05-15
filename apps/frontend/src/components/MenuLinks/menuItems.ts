53f4c403e99ea3bdea041b0cbde1bc7cd503536e
ticinovicsara <ticinovicsara0@gmail.com>
2025-05-15 19:35:21 +0200

    add links to hamburger menu

--- apps/frontend/src/components/MenuLinks/menuItems.ts
+++ apps/frontend/src/components/MenuLinks/menuItems.ts
@@ -36,23 +36,34 @@ export const menuItems: MenuItem[] = [
     roles: [UserRoles.Candidate],
   },
   {
-    label: "Tvoj raspored",
+    label: "Kalendar",
     to: routes.INSTRUCTOR_CALENDAR,
     roles: [UserRoles.Instructor],
   },
   {
-    label: "Admin panel",
-    to: routes.SCHOOL_ADMIN_DASHBOARD,
-    roles: [UserRoles.Admin, UserRoles.SchoolAdmin],
+    label: "Unesi slobodne termine",
+    to: routes.INSTRUCTOR_CALENDAR,
+    roles: [UserRoles.Instructor],
+  },
+
+  {
+    label: "Lista kandidata",
+    to: routes.INSTRUCTOR_CANDIDATE_LIST,
+    roles: [UserRoles.Instructor],
   },
   {
-    label: "Upravljanje korisnicima",
+    label: "Dodijeli instruktore",
     to: routes.SCHOOL_ADMIN_ASSIGN_INSTRUCTOR,
     roles: [UserRoles.Admin, UserRoles.SchoolAdmin],
   },
   {
+    label: "Odobri zahtjeve",
+    to: routes.PENDING_LIST,
+    roles: [UserRoles.Admin, UserRoles.SchoolAdmin],
+  },
+  {
     label: "Postavke profila",
-    to: routes.HOME,
+    to: routes.HOME, //treba dodat profile settings page
     roles: [
       UserRoles.Candidate,
       UserRoles.Instructor,
