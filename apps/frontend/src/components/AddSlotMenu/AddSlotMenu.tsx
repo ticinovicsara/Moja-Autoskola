717661b989115a89c5964a4ee10bee37d5621559
Luka <llesko00@fesb.hr>
2025-05-15 17:51:50 +0200

    Use already existing confirm popup

--- apps/frontend/src/components/AddSlotMenu/AddSlotMenu.tsx
+++ apps/frontend/src/components/AddSlotMenu/AddSlotMenu.tsx
@@ -1,28 +1,20 @@
 import { FC, useState } from "react";
 import styles from "./AddSlot.module.css";
 import { Cross } from "@/assets/svgs";
-import { InputField } from "@/components";
+import { ConfirmationPopup, InputField } from "@/components";
 import usePostInstructorSlot from "@/api/instructor/usePostInstructorSlot";
 import { AddInstructorSlotReq } from "@/types";
 import { useAuth } from "@/hooks";
-import { getDayFromDate, getMonthFromDate } from "@/utils";
+import { getFormattedTime } from "@/utils";
 
 interface AddSlotMenuProps {
     toggleAddSlotMenu: () => void;
 }
 
-type SlotForm = {
-    date: Date;
-    time: string;
-};
-
 const AddSlotMenu: FC<AddSlotMenuProps> = ({ toggleAddSlotMenu }) => {
     const { user } = useAuth();
     const { addInstructorSlot } = usePostInstructorSlot();
-    const [formData, setFormData] = useState<SlotForm>({
-        date: new Date(),
-        time: "12:00",
-    });
+    const [startTime, setStartTime] = useState<Date>(new Date());
     const [isConfirmPopupOpen, setIsConfirmPopupOpen] = useState(false);
 
     function toggleConfirmPopup() {
@@ -30,11 +22,7 @@ const AddSlotMenu: FC<AddSlotMenuProps> = ({ toggleAddSlotMenu }) => {
     }
 
     function handleSubmit() {
-        const startTime = new Date(formData.date);
-        const [hours, minutes] = formData.time.split(":").map(Number);
-        startTime.setHours(hours, minutes, 0, 0);
         const endTime = new Date(startTime);
-        endTime.setHours(endTime.getHours());
         endTime.setMinutes(endTime.getMinutes() + 45);
         const newSlot: AddInstructorSlotReq = {
             instructorId: user?.id ?? "",
@@ -45,28 +33,21 @@ const AddSlotMenu: FC<AddSlotMenuProps> = ({ toggleAddSlotMenu }) => {
         toggleConfirmPopup();
     }
 
-    function setDate(date: string) {
-        const parsedDate = new Date(date);
-        setFormData((prev) => ({
-            ...prev,
-            date: parsedDate,
-        }));
+    function setDate(dateString: string) {
+        const newDate = new Date(dateString);
+        const updatedDate = new Date(startTime);
+        updatedDate.setFullYear(newDate.getFullYear());
+        updatedDate.setMonth(newDate.getMonth());
+        updatedDate.setDate(newDate.getDate());
+        setStartTime(updatedDate);
     }
 
-    function setTime(time: string) {
-        setFormData((prev) => ({
-            ...prev,
-            time,
-        }));
-    }
-
-    function getEndTimeString(time: string): string {
-        const [hours, minutes] = time.split(":").map(Number);
-        const date = new Date();
-        date.setHours(hours, minutes + 45, 0, 0);
-        const endHours = date.getHours().toString().padStart(2, "0");
-        const endMinutes = date.getMinutes().toString().padStart(2, "0");
-        return `${endHours}:${endMinutes}`;
+    function setTime(timeString: string) {
+        const [hours, minutes] = timeString.split(":").map(Number);
+        const updatedTime = new Date(startTime);
+        updatedTime.setHours(hours);
+        updatedTime.setMinutes(minutes);
+        setStartTime(updatedTime);
     }
 
     return (
@@ -80,13 +61,13 @@ const AddSlotMenu: FC<AddSlotMenuProps> = ({ toggleAddSlotMenu }) => {
                     <div>
                         <InputField
                             type="date"
-                            value={formData.date.toISOString().split("T")[0]}
+                            value={startTime.toISOString().split("T")[0]}
                             onChange={setDate}
                             placeholder="Odaberi datum"
                         />
                         <InputField
                             type="time"
-                            value={formData.time}
+                            value={getFormattedTime(startTime)}
                             onChange={setTime}
                             placeholder="Vrijeme termina"
                         />
@@ -94,36 +75,15 @@ const AddSlotMenu: FC<AddSlotMenuProps> = ({ toggleAddSlotMenu }) => {
                     <button className="authBtn" onClick={toggleConfirmPopup}>
                         KREIRAJ
                     </button>
-                </div>
+                </div>{" "}
                 {isConfirmPopupOpen && (
-                    <div className={styles.confirmPopupContainer}>
-                        <div className={`${styles.confirmPopup} container`}>
-                            <img
-                                src={Cross}
-                                alt="cross"
-                                onClick={toggleConfirmPopup}
-                            />
-                            <p>
-                                <span>
-                                    {formData.date.getDate()}.{" "}
-                                    {getMonthFromDate(formData.date)},{" "}
-                                </span>
-                                {getDayFromDate(formData.date)}
-                                <br />
-                                {formData.time} -{" "}
-                                {getEndTimeString(formData.time)}
-                                <br />
-                                <br />
-                            </p>
-                            <p>
-                                Jesi li siguran da želiš kreirati ovaj termin?
-                            </p>
-                            <div className={styles.options}>
-                                <button onClick={handleSubmit}>Da</button>
-                                <button onClick={toggleConfirmPopup}>Ne</button>
-                            </div>
-                        </div>
-                    </div>
+                    <ConfirmationPopup
+                        handleConfirm={handleSubmit}
+                        togglePopup={toggleConfirmPopup}
+                        prompt="Jesi li siguran da želiš kreirati ovaj termin?"
+                        timeSlot={startTime}
+                        lengthInMin={45}
+                    />
                 )}
             </div>
         </div>
--- apps/frontend/src/components/ConfirmationPopup/ConfirmationPopup.tsx
+++ apps/frontend/src/components/ConfirmationPopup/ConfirmationPopup.tsx
@@ -1,44 +1,62 @@
+import { getDayFromDate, getFormattedTime, getMonthFromDate } from "@/utils";
 import styles from "./confirmation.module.css";
 import { Cross } from "@/assets/svgs";
 
 type ConfirmationProps = {
-  timeSlot?: string;
-  prompt: string;
-  handleConfirm: () => void;
-  togglePopup: () => void;
+    timeSlot?: Date;
+    lengthInMin?: number;
+    prompt: string;
+    handleConfirm: () => void;
+    togglePopup: () => void;
 };
 
 export const ConfirmationPopup = ({
-  timeSlot,
-  prompt,
-  handleConfirm,
-  togglePopup,
+    timeSlot,
+    lengthInMin,
+    prompt,
+    handleConfirm,
+    togglePopup,
 }: ConfirmationProps) => {
-  return (
-    <div className={styles.confirmPopupContainer}>
-      <div className={`${styles.confirmPopup} container`}>
-        <img
-          src={Cross}
-          alt="cross"
-          onClick={(e) => {
-            e.stopPropagation();
-            togglePopup();
-          }}
-        />
-        <p className={styles.timeSlot}> {timeSlot}</p>
-        <p>{prompt}</p>
-        <div className={styles.options}>
-          <button onClick={handleConfirm}>Da</button>
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              togglePopup();
-            }}
-          >
-            Ne
-          </button>
+    return (
+        <div className={styles.confirmPopupContainer}>
+            <div className={`${styles.confirmPopup} container`}>
+                <img
+                    src={Cross}
+                    alt="cross"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        togglePopup();
+                    }}
+                />
+                {timeSlot && (
+                    <p className={styles.timeSlot}>
+                        <span>
+                            {timeSlot.getDate()}. {getMonthFromDate(timeSlot)}
+                            ,{" "}
+                        </span>
+                        {getDayFromDate(timeSlot)}
+                        <br />
+                        {getFormattedTime(timeSlot)} -{" "}
+                        {getFormattedTime(
+                            new Date(timeSlot.getTime() + lengthInMin! * 60000)
+                        )}
+                        <br />
+                        <br />
+                    </p>
+                )}
+                <p>{prompt}</p>
+                <div className={styles.options}>
+                    <button onClick={handleConfirm}>Da</button>
+                    <button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            togglePopup();
+                        }}
+                    >
+                        Ne
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
